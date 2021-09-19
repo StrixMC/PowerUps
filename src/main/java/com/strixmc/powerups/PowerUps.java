a9bc0d92f5107df79c5cf3508e00fd51d15b0d6d
Sliide_ <yiyoperez99@gmail.com>
2021-09-19 00:33:21 -0300

    Project starting content.

---
+++ src/main/java/com/strixmc/powerups/PowerUps.java
@@ -0,0 +1,14 @@
+package com.strixmc.powerups;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PowerUps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
