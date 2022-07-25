d0aa2189f7ff2658a7f06c3732f0c996fa28eb48
Sliide_ <yiyoperez99@gmail.com>
2022-07-25 00:29:00 -0400

    [no ci] BUMP deps versions.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -3,17 +3,19 @@ import org.apache.tools.ant.filters.ReplaceTokens;
 val libsPackage = property("libsPackage") as String
 
 plugins {
-    id("com.github.johnrengelman.shadow") version ("7.0.0")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 dependencies {
     api(project(":api"))
     compileOnly("me.clip:placeholderapi:2.10.10")
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
-    compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.0")
+    compileOnly("com.github.decentsoftware-eu:decentholograms:2.5.1")
+    compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.9")
 
     implementation("com.github.StrixMC:ACID:1.2.0")
-    implementation("me.carleslc.Simple-YAML:Simple-Yaml:ed30ff3e6b")
+    implementation("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8")
+    implementation("me.fixeddev:commandflow-bukkit:0.5.2")
 }
 
 tasks {
